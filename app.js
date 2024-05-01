8aaa97611a68b9e9f063207d2119b786a9c6dc85
Teertharaj Turkar <teertharajturkar@gmail.com>
2024-05-01 10:37:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const typedTextSpan = document.querySelector("#word");
+const cursor = document.querySelector(".cursor");
+
+const words = ["web development", "C Programming", "Python", "C++", "Java", "DSA","machine Learning","Data Science"];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newLetterDelay = 500;
+let index = 0;
+let charIndex = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (words.length) {
+    setTimeout(type, newLetterDelay);
+  }
+});
+
+function type() {
+  if (charIndex < words[index].length) {
+    typedTextSpan.textContent += words[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newLetterDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = words[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    index++;
+    if (index >= words.length) {
+      index = 0;
+    }
+    setTimeout(type, typingDelay + 1100);
+  }
+}
